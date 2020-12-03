be90325777413946d4d2d03f5ba0bd63292b7d7d
Danielle Stauffer <72706124+dagibbins186@users.noreply.github.com>
2020-12-03 15:44:10 -0800

    Delete PyPoll.py

--- Data Analysis/Analysis/PyPoll.py
+++
@@ -1,7 +0,0 @@
-file_to_load='Resources/12032020-election_results_DS.csv'
-#Open the election results and read the file
-with open(file_to_load) as election_data:
-    #Perform analysis
-    print(election_data)
-#Close the file
-election_data.close()
